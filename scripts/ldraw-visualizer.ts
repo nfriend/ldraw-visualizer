16a6c6eb1308b597e9116ec2c0dbf4b43a6ec037
Nathan Friend <hello@nathanfriend.io>
2015-06-14 17:11:09 -0500

    updated background color

--- scripts/ldraw-visualizer.ts
+++ scripts/ldraw-visualizer.ts
@@ -14,17 +14,17 @@ module LdrawVisualizer {
 
 	function init() {
 		camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-		camera.position.z = 500;
-
+		camera.position.z = 200;
+		
 		controls = new THREE.OrbitControls(camera);
 		controls.damping = 0.2;
 		controls.addEventListener('change', render);
 
 		scene = new THREE.Scene();
-		scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+		scene.fog = new THREE.FogExp2(0x111111, 0.002);
 
-		var geometry = new THREE.BoxGeometry(100, 100, 100);
-		var material = new THREE.MeshLambertMaterial({ color: 0xffffff, shading: THREE.FlatShading });
+		var geometry = new THREE.BoxGeometry(50, 50, 50);
+		var material = new THREE.MeshLambertMaterial({ color: 0xff0000, shading: THREE.FlatShading });
 		var cube = new THREE.Mesh(geometry, material);
 		cube.position.x = 0;
 		cube.position.y = 0;
@@ -45,14 +45,14 @@ module LdrawVisualizer {
 
 		// lights
 		var directionalLight = new THREE.DirectionalLight(0xffffff);
-		directionalLight.position.set(1, .6, .2);
+		directionalLight.position.set(1, .6, -.2);
 		scene.add(directionalLight);
 
-		directionalLight = new THREE.DirectionalLight(0x002288);
-		directionalLight.position.set(-1, -1, -1);
+		directionalLight = new THREE.DirectionalLight(0xffffff);
+		directionalLight.position.set(-1, -1.5, 1.7);
 		scene.add(directionalLight);
 
-		var ambientLight = new THREE.AmbientLight(0x222222);
+		var ambientLight = new THREE.AmbientLight(0x000000);
 		scene.add(ambientLight);
 		
 		// renderer
@@ -79,6 +79,6 @@ module LdrawVisualizer {
 	}
 
 	function render() {
-		renderer.render(scene, camera); 
+		renderer.render(scene, camera);
 	}
 }
